65805e3cb8465006bbae4ea10a474162ff204118
Chaitanya Pathipati <cp13@iitbbs.ac.in>
2021-01-27 23:03:31 +0530

    added board basic mechanics

--- boxes.ui/src/features/board/Board.tsx
+++ boxes.ui/src/features/board/Board.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Board.css'
 
 interface BoardProps {
@@ -8,21 +8,55 @@ interface BoardProps {
 export function Board({ size }: BoardProps) {
     let board = [];
     for (let i = 0; i < size; i++) {
-        let row = [];
-        for (let j = 0; j < size; j++) {
-            row.push(<Dot x={j} y={i} />);
-        }
-        board.push(
-            <div className="row" key={i}>
-                {row}
-            </div>
-        );
+        board.push(horizontalRow(i));
+        board.push(verticalRow(i));
     }
+    board.push(horizontalRow(size));
     return (
         <div id="game-board">
             {board}
         </div>
     );
+
+    function verticalRow(i: number) {
+        let row = [];
+        for (let j = 0; j < size; j++) {
+            let id = `(${i},${j})`;
+            row.push(
+                <>
+                    <Line  key={id + "-vline"}/>
+                    <Square key={id + "-vsquare"} />
+                </>
+            );
+        }
+        let id = `(${i},${size})`;
+        row.push(<Line key={id + "-vline"}/>);
+        return (
+            <div className="row" key={i + "-vertical"}>
+                {row}
+            </div>
+        );
+    }
+
+    function horizontalRow(i: number) {
+        let row = [];
+        for (let j = 0; j < size; j++) {
+            let id = `(${i},${j})`;
+            row.push(
+                <>
+                    <Dot x={j} y={i} key={id + "-dot"} />
+                    <Line horizontal key={id + "-line"} />
+                </>
+            );
+        }
+        let id =`(${i},${size})-dot`;
+        row.push(<Dot x={size} y={i} key={id} />);
+        return (
+            <div className="row" key={i + "-horizontal"}>
+                {row}
+            </div>
+        );
+    }
 }
 
 
@@ -30,11 +64,40 @@ interface DotProps {
     x: number;
     y: number;
 }
-export function Dot(props: DotProps) {
-    let id = props.x + "," + props.y
-    return (
-        <div className="dot" key={id}>
 
+export function Dot(props: DotProps) {
+    
+    return (
+        <div className="dot">
+        </div>
+    );
+}
+
+interface LineProps {
+    horizontal?: boolean;
+
+}
+
+export function Line(props: LineProps){
+    const [isSelected, setSelected] = useState(false);
+    return (
+        <div 
+            className={ "line" + 
+                        (props.horizontal ? " horizontal-line" : " vertical-line") +
+                        (isSelected ? " line-selected" : "")}
+            onClick={e => handleLineClicked()}
+        >
+        </div>
+    );
+
+    function handleLineClicked(){
+        setSelected(true);
+    }
+}
+
+export function Square(){
+    return (
+        <div className="square">
         </div>
     );
 }
